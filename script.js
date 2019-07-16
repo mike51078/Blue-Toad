9d838b238fcd1ba2f07fb47c6aabed031b1df81c
elcid00 <elcid00@aol.com>
2019-07-16 16:41:30 -0400

    almost finished

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const client_id = '08d4350b8ed71cbceaa67f01b5c01ae2b2fb14cc75fe601c63cfa42256a1d
 const api_url = `https://api.unsplash.com/search/photos?page=1&per_page=25&client_id=${client_id}`;
 const form = document.querySelector('form');
 const input = document.querySelector('input');
-const imageSection = document.querySelector('.images');
+const photoSection = document.querySelector('.photos');
 
 form.addEventListener('submit', formSubmitted);
 
@@ -11,17 +11,25 @@ function formSubmitted(event) {
 	let searchTerm = input.value;
 
 	searchStart();
-	search(searchTerm).then(displayImages);
+	search(searchTerm).then(displayPhotos);
 }
 
 function searchStart() {
-	imageSection.innerHTML = '';
+	photoSection.innerHTML = '';
 }
 
 function search(searchTerm) {
 	let url = `${api_url}&query=${searchTerm}`;
 	return fetch(url).then((response) => response.json()).then((result) => {
-		console.log(result.results);
 		return result.results;
+	});
+}
+
+function displayPhotos(photos) {
+	photos.forEach((photo) => {
+		let photoContainer = document.createElement('div');
+		photoContainer.className = 'PhotoResult';
+		photoContainer.innerHTML = `<img src="${photo.urls.regular}">`;
+		photoSection.appendChild(photoContainer);
 	});
 }
